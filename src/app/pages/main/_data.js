45e4bbc69601c101db9a2f73f1d7a225d460c345
Disorder <disorrder@gmail.com>
2017-12-17 02:26:08 +0300

    WIP main

--- src/app/pages/main/_data.js
+++ src/app/pages/main/_data.js
@@ -7,8 +7,8 @@ module.exports = {
     ],
 
     carouselItems: [
-        {url: '#', img: require("assets/images/links/right1.png")},
-        {url: '#', img: require("assets/images/links/right2.png")},
-        {url: '#', img: require("assets/images/links/right3.png")}
+        {url: '#', img: require("assets/images/carousel/carousel-1.png")},
+        {url: '#', img: require("assets/images/carousel/carousel-1.png")},
+        {url: '#', img: require("assets/images/carousel/carousel-1.png")}
     ]
 };
