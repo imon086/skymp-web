a8a7e8436ac2683b924945be2be49846efebcf70
Disorder <disorrder@gmail.com>
2017-12-18 00:22:50 +0300

    main page almost ready
    грёбаные костыли

--- src/app/pages/main/_data.js
+++ src/app/pages/main/_data.js
@@ -1,4 +1,6 @@
 module.exports = {
+    ENV: process.env.NODE_ENV,
+    WEBPACK: process.env.WEBPACK,
     sidebarLinks: [
         {url: '#', img: require("assets/images/links/right1.png")},
         {url: '#', img: require("assets/images/links/right2.png")},
@@ -10,5 +12,16 @@ module.exports = {
         {url: '#', img: require("assets/images/carousel/carousel-1.png")},
         {url: '#', img: require("assets/images/carousel/carousel-1.png")},
         {url: '#', img: require("assets/images/carousel/carousel-1.png")}
-    ]
+    ],
+
+    articles: []
 };
+
+for (let i=1; i <5; i++) {
+    module.exports.articles.push({
+        id: i,
+        title: 'Title '+i,
+        lead: 'Lead '+i,
+        fullText: 'Full text '+i
+    })
+}
