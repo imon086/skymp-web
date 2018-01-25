c9811f90898660b8e72ffe421e190e0fada1087d
Disorder <disorrder@gmail.com>
2018-01-26 01:04:39 +0300

    Fix Semantics

--- src/app/pages/main/_data.js
+++ src/app/pages/main/_data.js
@@ -23,5 +23,5 @@ for (let i=1; i <5; i++) {
         title: 'Title '+i,
         lead: 'Lead '+i,
         fullText: 'Full text '+i
-    })
+    });
 }
--- src/app/pages/main/index.js
+++ src/app/pages/main/index.js
@@ -2,6 +2,7 @@ import 'app/vendor.js';
 import 'app/style.styl';
 
 import 'app/components/navbar';
+import 'app/components/footer';
 
 import '../components.styl';
 import './style.styl';
