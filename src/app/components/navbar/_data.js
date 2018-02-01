1b3dbadae03944a23e6a925a8f8d5a6888b76ad8
Disorder <disorrder@gmail.com>
2018-02-01 19:44:24 +0300

    About - 2nd slide finished

--- src/app/components/navbar/_data.js
+++ src/app/components/navbar/_data.js
@@ -2,9 +2,16 @@ module.exports = {
     menu: [
         {name: 'main', url: '/', title: 'Новости'},
         {name: 'forum', url: 'http://skymp.ru/forum/', title: 'Форум'},
-        {name: 'about', url: '/about.html', title: 'Особенности'},
+        {name: 'about', url: '/about', title: 'Особенности'},
         {name: 'support', url: '#support', title: 'Поддержка'},
         {name: 'shop', url: '#shop', title: 'Магазин'},
         {name: 'profile', url: '#profile', title: 'Профиль'}
     ]
 };
+
+const staticPages = true;
+if (staticPages) {
+    module.exports.menu.forEach((v) => {
+        if (v.url[0] === '/' && v.url.length > 1) v.url += '.html';
+    });
+}
--- src/app/pages/about/_data.js
+++ src/app/pages/about/_data.js
@@ -2,4 +2,72 @@ module.exports = {
     ENV: process.env.NODE_ENV,
     WEBPACK: process.env.WEBPACK,
 
+    newbieCarousel: [
+        {
+            title: 'Шахты и фермы',
+            icon1: require('./assets/icons/farm.svg'),
+            icon2: require('./assets/icons/mine.svg'),
+            text: [
+                'Добывай свои первые септимы:',
+                'маши киркой, собирай урожай.',
+                'Перед тобой откроется множество возможностей!'
+            ].join('\n'),
+        },
+        {
+            title: 'Крафт и торговля',
+            icon1: require('./assets/icons/craft.svg'),
+            icon2: require('./assets/icons/pickpocket.svg'),
+            text: [
+                'Добывай свои первые септимы:',
+                'маши киркой, собирай урожай.',
+                'маши киркой, собирай урожай.',
+                'Перед тобой откроется множество возможностей!'
+            ].join('\n'),
+        },
+        {
+            title: 'Магия и снаряжение',
+            icon1: require('./assets/icons/mage.svg'),
+            icon2: require('./assets/icons/sword.svg'),
+            text: [
+                'Добывай свои первые септимы:',
+                'маши киркой, собирай урожай.',
+                'маши киркой, собирай урожай.',
+                'маши киркой, собирай урожай.',
+                'Перед тобой откроется множество возможностей!'
+            ].join('\n'),
+        },
+        {
+            title: 'Гильдии и фракции',
+            icon1: require('./assets/icons/guild-dark.svg'),
+            icon2: require('./assets/icons/guild-thief.svg'),
+            text: [
+                'Добывай свои первые септимы:',
+                'маши киркой, собирай урожай.',
+                'маши киркой, собирай урожай.',
+                'маши киркой, собирай урожай.',
+                'маши киркой, собирай урожай.',
+                'Перед тобой откроется множество возможностей!'
+            ].join('\n'),
+        },
+        {
+            title: 'ПВЕ и ПВП контент',
+            icon1: require('./assets/icons/bow.svg'),
+            icon2: require('./assets/icons/flags.svg'),
+            text: [
+                'Добывай свои первые септимы:',
+                'маши киркой, собирай урожай.',
+                'Перед тобой откроется множество возможностей!'
+            ].join('\n'),
+        },
+        {
+            title: 'Общение и социум',
+            icon1: require('./assets/icons/speech.svg'),
+            icon2: require('./assets/icons/family.svg'),
+            text: [
+                'Добывай свои первые септимы:',
+                'маши киркой, собирай урожай.',
+                'Перед тобой откроется множество возможностей!'
+            ].join('\n'),
+        }
+    ]
 };
